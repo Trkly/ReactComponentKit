2949623a1cc311426df721894c3b8c43220f02a0
zzk <Acapplella@163.com>
2024-03-24 22:31:55 +0800

    完成Icon组件和Transition组件开发

--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -2,7 +2,8 @@ import React, { useContext, useState, FunctionComponentElement } from 'react'
 import classNames from 'classnames'
 import { MenuContext } from './menu'
 import { MenuItemProps } from './menuItem'
-
+import Icon from '../Icon/icon'
+import Transition from '../Transition/transition'
 export interface SubMenuProps {
   index?: string
   title: string
@@ -24,7 +25,9 @@ const SubMenu: React.FC<SubMenuProps> = ({
       : false
   const [menuOpen, setOpen] = useState(isOpend)
   const classes = classNames('menu-item submenu-item', className, {
-    'is-active': context.index === index
+    'is-active': context.index === index,
+    'is-opened': menuOpen,
+    'is-vertical': context.mode === 'vertical'
   })
   const handleClick = (e: React.MouseEvent) => {
     e.preventDefault()
@@ -69,12 +72,17 @@ const SubMenu: React.FC<SubMenuProps> = ({
         console.error('Warning: SubMenu has a child which is not a MenuItem')
       }
     })
-    return <ul className={subMenuClasses}>{childrenComponent}</ul>
+    return (
+      <Transition in={menuOpen} timeout={300} animation="zoom-in-top">
+        <ul className={subMenuClasses}>{childrenComponent}</ul>
+      </Transition>
+    )
   }
   return (
     <li key={index} className={classes} {...hoverEvents}>
       <div className="submenu-title" {...clickEvents}>
         {title}
+        <Icon icon="angle-down" className="arrow-icon" />
       </div>
       {renderChildren()}
     </li>
